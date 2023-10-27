61a3b8e6bc097b20014053ae87f70380365da6fa
Guilherme Painko Scalcon <88914560+GPainko@users.noreply.github.com>
2023-10-27 20:54:43 -0300

    Add files via upload

---
+++ Trabalho_matematica/trabalho.py
@@ -0,0 +1,15 @@
+import math
+#função que para calcular o aranjo n p a p
+def calcular_arranjo(n,r):
+    return math.factorial(n)/math.factorial(n-r)
+    #biblioteca math utilizada para calcular o fatorial.
+
+def calcular_permutacao(n):
+    return math.factorial(n)
+
+#exemplo de uso:
+n = int(input("digite o numero de elementos: "))
+r = int(input("digite o numero de combinação: "))
+
+print(f"O Arranjo de {n}, {r} a {r} é {calcular_arranjo(n,r)}" )
+print(f"O Combinação de {n}, {r} a {r} é {calcular_permutacao(n)}" )
